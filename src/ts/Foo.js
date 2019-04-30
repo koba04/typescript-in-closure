c7ac8b024f2f0cf42404b63ba37b744e73ba22ba
Toru Kobayashi <koba0004@gmail.com>
2019-05-01 00:30:54 +0900

    use react installed from npm

--- src/ts/Foo.js
+++ src/ts/Foo.js
@@ -1,12 +1,17 @@
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 /**
  * @fileoverview added by tsickle
  * @suppress {checkTypes,extraRequire,missingOverride,missingReturn,unusedPrivateMembers,uselessCode} checked by tsc
  */
 goog.module('Foo.tsx');
 var module = module || { id: 'Foo.tsx' };
-/** @type {function(): ?} */
+var tsickle_react_1 = goog.requireType("react");
+var react_1 = __importDefault(require("react"));
+/** @type {function(): !JSX.Element} */
 var Foo = (/**
- * @return {?}
+ * @return {!JSX.Element}
  */
-function () { return React.createElement("div", null, "Hello"); });
+function () { return react_1.default.createElement("div", null, "Hello"); });
 exports.default = Foo;
