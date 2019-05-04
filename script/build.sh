1ea18d6d25be8e45979f7988a96cc7fd209bf9d5
Toru Kobayashi <koba0004@gmail.com>
2019-05-04 16:05:25 +0900

    build: use google-closure-compiler instead of closurebuilder.py

--- script/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-./node_modules/google-closure-library/closure/bin/build/closurebuilder.py \
---namespace=app \
---root=. \
---output_file=public/bundle.js \
---output_mode=compiled \
---compiler_jar=node_modules/google-closure-compiler-java/compiler.jar \
---compiler_flags="--compilation_level=ADVANCED_OPTIMIZATIONS --externs=externs.js"
